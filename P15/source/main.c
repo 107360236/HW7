719ec22eb3085eb6277cdb8acb51e271740c8e99
107360236 <a0982784312@gmail.com>
2018-12-25 22:20:23 +0800

    HW7

---
+++ P15/source/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE *fptr1, *fptr2;
+	int err1,err2;
+	char ch;
+
+	err1 = fopen_s(&fptr1, "C://c_code/HW7/P15/welcome.txt", "r");
+	err2 = fopen_s(&fptr2, "C://c_code/HW7/P15/output.txt", "w");
+	if ((fptr1 != NULL)&&(fptr2!=NULL))
+	{
+		while ((ch = getc(fptr1)) != EOF)
+			putc(ch,fptr2);
+		fclose(fptr1);
+		fclose(fptr2);
+		printf("檔案拷貝完成!!\n");
+	}
+	else
+		printf("檔案開啟失敗!!\n");
+	system("pause");
+	return 0;
+}
